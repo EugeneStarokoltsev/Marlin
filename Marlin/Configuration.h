446acf4aaf88cbcde517f02537d30ae2dc286c54
Eugene Starokoltsev <evgeny.starokoltsev@gmail.com>
2016-11-05 22:45:19 +0300

    Limits reduced

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -627,8 +627,8 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS 200
-#define Y_MAX_POS 200
-#define Z_MAX_POS 200
+#define Y_MAX_POS 180
+#define Z_MAX_POS 190
 
 //===========================================================================
 //========================= Filament Runout Sensor ==========================
